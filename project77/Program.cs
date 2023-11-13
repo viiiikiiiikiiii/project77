e4570fbb5d2669655fc9d1f22874842eeacbdfc0
Admin <Admin@WIN-TU5QH3C8BGU>
2023-11-13 19:10:48 +0300

    Добавьте файлы проекта.

---
+++ project77/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+class Programm
+{
+    public static void Main()
+    {
+        WriteLine("Введите X");
+        double x = Convert.ToDouble(ReadLine());
+        WriteLine(8 * Math.Pow(x - 3, 6) - 7 * Math.Pow(x - 3, 3) + 27);
+    }
+}
